e2d35343149bedcfbb006b2fd0f0b553ee4b6d41
Emlyn Corrin <emlyn.corrin@gmail.com>
2022-05-11 20:58:04 +0100

    Improve error handling, fix login error

--- notifier.py
+++ notifier.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import requests
+import traceback
 from html.parser import HTMLParser
 from twilio.rest import Client
 
@@ -15,41 +16,66 @@ class WiseParser(HTMLParser):
         super().__init__()
         self._active = False
         self._data = []
+        self._error = False
+        self._errs = []
+        self._date = None
+        self._time = None
+        self._balance = None
         self.feed(txt)
+        self._done()
 
     def handle_starttag(self, tag, attrs):
         if tag == 'div':
             for k, v in attrs:
-                if k == 'class' and 'cashless-home-balance-sml' in v.split(' '):
+                if k == 'class' and 'cashless-home-balance-big' in v.split(' '):
                     self._data.append('')
                     self._active = True
+                    break
+        elif tag == 'td':
+            for k, v in attrs:
+                if k == 'class' and 'error_form_data' in v.split(' '):
+                    self._errs.append('')
+                    self._error = True
                     break
 
     def handle_endtag(self, tag):
         if self._active and tag == 'div':
             self._active = False
+        elif self._error and tag == 'td':
+            self._error = False
 
     def handle_data(self, data):
         if self._active:
             self._data[-1] += data
+        if self._error:
+            self._errs[-1] += data
+
+    def _done(self):
+        errtxt = '\n'.join(e.strip()
+                           for e in '\n'.join(self._errs).split('\n')
+                           if e.strip())
+        if errtxt:
+            raise Exception(errtxt)
+        if len(self._data) < 4:
+            raise Exception(f"Unexpected response from WisePay ({len(self._data)} cells)")
+        m = self.BALANCE_PATTERN.match(self._data[3])
+        if not m:
+            raise Exception(f"Unexpected balance from WisePay: {self._data[3]}")
+        self._date = self._data[1]
+        self._time = self._data[2]
+        self._balance = float(m.group(1))
 
     @property
     def date(self):
-        assert len(self._data) >= 2
-        return self._data[1]
+        return self._date
 
     @property
     def time(self):
-        assert len(self._data) >= 3
-        return self._data[2]
+        return self._time
 
     @property
     def balance(self):
-        assert len(self._data) >= 4
-        m = self.BALANCE_PATTERN.match(self._data[3])
-        if not m:
-            raise Exception(f"Invalid balance format: {self._data[3]}")
-        return float(m.group(1))
+        return self._balance
 
 
 def wisepay_state(mid, login, pw):
@@ -83,12 +109,12 @@ def send_notification(account_sid, auth_token, phone_number, message):
         from_=whatsapp('+14155238886'),
         to=whatsapp(phone_number))
 
-def main(phone_number, threshold):
+def main(phone_number, threshold=None):
     status = 0
     wp = None
     try:
         wp_mid = os.getenv('INPUT_WISEPAY_MID')
-        wp_login = os.getenv('INPUT_WISEPAY_USER')
+        wp_login = os.getenv('INPUT_WISEPAY_LOGIN')
         wp_pw = os.getenv('INPUT_WISEPAY_PASSWORD')
         if threshold:
             threshold = float(threshold)
@@ -96,6 +122,7 @@ def main(phone_number, threshold):
 
         message = f"WisePay balance: £{wp.balance:0.02f} on {wp.date} at {wp.time}"
     except Exception as e:
+        traceback.print_exc()
         message = f"WisePay error: {e}"
         status = 1
 
